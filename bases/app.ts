cf5cbc97a795b9e66249ce71d80f7c09175fd420
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-12-26 06:25:52 -0600

    Booleans

--- bases/app.ts
+++ bases/app.ts
@@ -1,11 +1,5 @@
+(() => {
+    const a:number = 10;
 
-const msg: string = 'Hola mundo';
-
-const hero = {
-    name: 'Ironman',
-    age: 45
-}
-
-hero.age = 50
-
-console.log( hero.age + 1 );
+    console.log(a);
+})()
