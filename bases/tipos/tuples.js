2b8984c21ee6cad8ac49f409690242ec4fe56614
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2024-12-27 05:27:20 -0600

    Tuples

---
+++ bases/tipos/tuples.js
@@ -0,0 +1,8 @@
+"use strict";
+(() => {
+    const hero = ['Dr Strange', 50, true];
+    hero[0] = 'IronMan';
+    hero[1] = 30;
+    hero[2] = false;
+    console.log(hero);
+})();
---
+++ bases/tipos/tuples.ts
@@ -0,0 +1,11 @@
+(() => {
+
+    const hero: [string, number, boolean] = ['Dr Strange', 50, true];
+    
+    hero[0] = 'IronMan';
+    hero[1] = 30
+    hero[2] = false;
+
+    console.log(hero);
+
+})()
