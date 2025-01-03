f264adfd3ef5b963cbca7bd54ea82e43baa100f0
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-01-03 00:20:09 -0600

    Tipos personalizados

--- bases/objetos/type.ts
+++ bases/objetos/type.ts
@@ -1,12 +1,19 @@
 (() => {
 
-    let flash: { name: string, age?: number, powers: string[],  getName?: () => string } = {
+    type Hero = {
+        name: string;
+        age?: number;
+        powers: string[];
+        getName?: () => string;
+    }
+
+    let flash: Hero = {
         name: 'Barry Allen',
         age: 24,
         powers: ['Velocidad', 'Viajar en el tiempo'],
     }
 
-    let superman: { name: string, age?: number, powers: string[],  getName?: () => string } = {
+    let superman: Hero = {
         name: 'Clark kent',
         age: 28,
         powers: ['Super fuerza'],
