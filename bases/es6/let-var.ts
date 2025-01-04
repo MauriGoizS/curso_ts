117d29b63f9d55c3298208045b6bb534a09f9a7b
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-01-04 00:29:41 -0600

    let-var

---
+++ bases/es6/let-var.ts
@@ -0,0 +1,12 @@
+(() => {
+
+    const nombre: string = 'Fernando';
+
+    const getname = (): void => {
+        console.log('Viejo nombre');
+    }
+
+    // getname = () => { console.log('Nuevo nombre');}
+
+    // getname();
+})()
