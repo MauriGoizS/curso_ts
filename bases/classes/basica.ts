d4dab4440649d2a9daffd98e97aee4e942818e94
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-01-07 23:53:14 -0600

    Metodos publicos y privados

--- bases/classes/basica.ts
+++ bases/classes/basica.ts
@@ -6,6 +6,9 @@
         // private team: string;
         // public realName?: string;
         static avgAge: number = 35;
+        static getAvAge() {
+            return this.name;
+        }
 
         constructor(
             private name: string,
@@ -13,10 +16,15 @@
             public realName?: string,
         ) {}
 
+        public bio() {
+            return `${ this.name } (${ this.team })`;
+        }
+
     }
 
     const antman: Avenger = new Avenger('AntMan', 'Capitan', 'Scott Lang');
-
     console.log( antman );
+
+    console.log( Avenger.getAvAge() );
 
 })()
--- bases/main.js
+++ bases/main.js
@@ -1,14 +1,21 @@
 "use strict";
 (() => {
     class Avenger {
+        static getAvAge() {
+            return this.name;
+        }
         constructor(name, team, realName) {
             this.name = name;
             this.team = team;
             this.realName = realName;
         }
+        bio() {
+            return `${this.name} (${this.team})`;
+        }
     }
     Avenger.avgAge = 35;
     const antman = new Avenger('AntMan', 'Capitan', 'Scott Lang');
     console.log(antman);
+    console.log(Avenger.getAvAge());
 })();
 //# sourceMappingURL=main.js.map
