5d800ed69eccdeceb4d95fc4c4097f9d20d56ba7
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-01-07 22:49:33 -0600

    Definicion de una clase de TS

--- bases/main.js
+++ bases/main.js
@@ -1,52 +1,14 @@
 "use strict";
-var __rest = (this && this.__rest) || function (s, e) {
-    var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
-        t[p] = s[p];
-    if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
+(() => {
+    class Avenger {
+        constructor(name, team, realName) {
+            this.name = name;
+            this.team = team;
+            this.realName = realName;
         }
-    return t;
-};
-(() => {
-    const avengers = {
-        nick: 'Samuel L. Jackson',
-        ironman: 'Robert Downey Jr.',
-        vision: 'Paul Bettany',
-        activo: true,
-        poder: 1500.324324,
-    };
-    const printAvenger = (_a) => {
-        var { ironman } = _a, resto = __rest(_a, ["ironman"]);
-        console.log(ironman, resto);
-    };
-    const avengersArr = ['Cap. America', true, 150.33];
-    const [capitan, ironman, seriaUnNumero] = avengersArr;
-})();
-(() => {
-    const ironman = {
-        name: 'Ironma',
-        weapon: 'Armorsuit'
-    };
-    const captainAmerica = {
-        name: 'Captain America',
-        weapon: 'Escudo'
-    };
-    const thor = {
-        name: 'Thor',
-        weapon: 'Mjolnir'
-    };
-    const avengers = [ironman, thor, captainAmerica];
-    for (const avenger of avengers) {
-        console.log(avenger);
     }
-})();
-(() => {
-    const nombre = 'Fernando';
-    const getname = () => {
-        console.log('Viejo nombre');
-    };
+    Avenger.avgAge = 35;
+    const antman = new Avenger('AntMan', 'Capitan');
+    console.log(antman);
 })();
 //# sourceMappingURL=main.js.map
