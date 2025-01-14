99a0d29d5a5eba4551b4604c05d3b74a09c1e9a3
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-01-13 22:44:02 -0600

    Ejemplo de funcion generica en acción

--- ts-node/src/index.ts
+++ ts-node/src/index.ts
@@ -1,12 +1,22 @@
-import { printObject, genericFunction, genericFunctionArrow } from "./generics.ts/generics";
+import { printObject, genericFunction, genericFunctionArrow } from './generics.ts/generics';
+import { Hero } from './interfaces/hero';
+import { villain } from './interfaces/villiain';
 
 // printObject( 23 );
 // printObject( new Date() );
 // printObject( { a:1, b:2, c:3, }  );
 // printObject( [1,2,3,4,5,6,7,8,9,10] );
 // printObject( 'Hola Mundo' );
-const name: string = 'Mauricio';
+// const name: string = 'Mauricio';
 
-console.log( genericFunctionArrow(3.1416).toFixed(2) );
-console.log( genericFunctionArrow( name ).toUpperCase() );
-console.log( genericFunctionArrow( new Date() ).getDate() );
+// console.log( genericFunctionArrow(3.1416).toFixed(2) );
+// console.log( genericFunctionArrow( name ).toUpperCase() );
+// console.log( genericFunctionArrow( new Date() ).getDate() );
+
+const deadpool = {
+    name: 'Deadpool',
+    realName: 'Wade Winston Wilson',
+    dangerLevel: 130,
+}
+
+console.log( genericFunctionArrow<villain>(deadpool ).dangerLevel );
