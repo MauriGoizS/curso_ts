193a55c99156d3371dcefc0a4d173159bc630994
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-01-16 00:45:36 -0600

    Trabajando con ts en lugar de js

--- express-api/index.ts
+++ express-api/index.ts
@@ -1,8 +1,15 @@
-const express = require("express");
+import express from 'express'
+
 const app = express();
 const port = 3000;
 
 app.get("/", (req, res) => {
+
+    res.status(201).json({
+        ok: false,
+        msg: 'Nuevo id: 123123123'
+    })
+
     res.json({
         ok: true,
         msg: 'Salio bien'
