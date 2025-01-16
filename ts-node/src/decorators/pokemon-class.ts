20b5108f0f43b96d90d7ad80d9a7a862064b2523
Mauricio Goiz Sarmiento <maurigoizsa@gmail.com>
2025-01-15 22:45:39 -0600

    Decoradores de Metodos

--- ts-node/src/decorators/pokemon-class.ts
+++ ts-node/src/decorators/pokemon-class.ts
@@ -11,13 +11,29 @@ const printToConsoleConditional = ( print: boolean = false ): Function => {
 }
 
 const bloquearPrototipo = function( constructor: Function ) {
-    // Object.seal( constructor )
-    // Object.seal( constructor.prototype )
+    Object.seal( constructor )
+    Object.seal( constructor.prototype )
+}
 
+function CheckValidPokemonId() {
+    return function( target: any, propertyKey: string, descriptor: PropertyDescriptor ) {
+        
+        const originalMethod = descriptor.value
+
+        descriptor.value = ( id: number ) => {
+            if ( id < 1 || id > 800 ) {
+                return console.error('El id del pokemon debe estar entre 1 y 800');
+            } else {
+                return originalMethod(id)
+            }
+        }
+        // descriptor.value = () => console.log('Hola mundo');
+
+    }
 }
 
 @bloquearPrototipo
-@printToConsoleConditional( true )
+@printToConsoleConditional( false )
 export class Pokemon {
 
     public publicApi: string = 'https://pokeapi.co'
@@ -25,4 +41,9 @@ export class Pokemon {
     constructor(
         public name: string
     ) {}
+
+    @CheckValidPokemonId()
+    savePokemonToDB ( id: number ) {
+        console.log(`Pokemon guardado en DB ${ id }`);
+    }
 }
